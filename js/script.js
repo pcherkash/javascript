e93d29b2584b1005de96abe59bbc5ae5a3a1a549
Павел <pavel@macbook-pro-pavel.local>
2022-05-27 07:21:48 +0000

    master

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use stcrict";
+
+//alert('Hello');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = prompt("Сколько лет","10")
+//console.log(answer);
+
+const answers = [];
+answers[0] = prompt('Имя', '');
+answers[1] = prompt('Фамилия', '');
+answers[2] = prompt('Возвраст', '');
+
+document.write(answers);
